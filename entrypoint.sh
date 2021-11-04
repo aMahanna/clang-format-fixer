3c419ceb995b93bd22d610a24652ed416da5d3f6
aMahanna <anthony.mahanna@gmail.com>
2021-11-03 23:04:43 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,34 +1,67 @@
-#!/bin/sh
+#!/bin/bash
+SOURCES=()
 
-set -eu
+function log() {
+    echo -e "[ gh-action ] :: $1"
+}
 
-# REPO_FULLNAME=$(jq -r ".repository.full_name" "$GITHUB_EVENT_PATH")
+function split_csv() {
+    IFS=','
+    csv_data="$1"
+    local -n global_list_array="$2"
+    for i in $csv_data; do
+        if [ -f "$i" ]; then
+            global_list_array+=("$i")
+        fi
+    done
+    unset IFS
+}
 
-# echo "## Initializing git repo..."
-# git init
-# echo "### Adding git remote..."
-# git remote add origin https://x-access-token:$GITHUB_TOKEN@github.com/$REPO_FULLNAME.git
-# echo "### git fetch $BRANCH ..."
-# git fetch origin $BRANCH
-# echo "### Branch: $BRANCH (ref: $GITHUB_REF )"
-# git checkout $BRANCH
+cd "$GITHUB_WORKSPACE" || exit 2
+log "Action started"
+log "Sources to check: $INPUT_SOURCES\n"
+split_csv "$INPUT_SOURCES" SOURCES
 
-echo "## Configuring git author..."
-git config --global user.email "clang-format@1337z.ninja"
-git config --global user.name "Clang Format"
+for file in "${SOURCES[@]}"; do
+    clang-format -style=file -i "${file}"
+done
 
 echo "### Getting branch"
 BRANCH=${GITHUB_REF#*refs/heads/}
-# Ignore workflow files (we may not touch them)
-git update-index --assume-unchanged .github/workflows/*
 
-echo "## Running clang-format on C/C++ source"
-SRC=$(git ls-tree --full-tree -r HEAD | grep -e "\.\(c\|h\|hpp\|cpp\)\$" | cut -f 2)
-
-clang-format -style=file -i $SRC
+echo "## Configuring git author..."
+git config --global user.email "anthony.mahanna@gmail.com"
+git config --global user.name "aMahanna"
 
 echo "## Commiting files..."
 git commit -a -m "apply clang-format" || true
 
 echo "## Pushing to $BRANCH"
 git push -u origin $BRANCH
+
+
+# #!/bin/sh
+
+# set -eu
+
+# echo "## Configuring git author..."
+# git config --global user.email "clang-format@1337z.ninja"
+# git config --global user.name "Clang Format"
+
+# echo "### Getting branch"
+# BRANCH=${GITHUB_REF#*refs/heads/}
+# # Ignore workflow files (we may not touch them)
+# git update-index --assume-unchanged .github/workflows/*
+
+# echo "## Running clang-format on C/C++ source"
+# SRC=$(git ls-tree --full-tree -r HEAD | grep -e "\.\(c\|h\|hpp\|cpp\)\$" | cut -f 2)
+
+# clang-format -style=file -i $SRC
+
+# echo "## Commiting files..."
+# git commit -a -m "apply clang-format" || true
+
+# echo "## Pushing to $BRANCH"
+# git push -u origin $BRANCH
+
+
