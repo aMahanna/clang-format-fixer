65f78e854ac51ca9a05f35f902fa4d5f11202f7a
aMahanna <anthony.mahanna@gmail.com>
2021-11-03 22:52:37 -0400

    temp

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
-FROM alpine
-#ENV clang_version=8.0.0-r0
-#LABEL description="Clang Format ${clang_version}"
-#RUN apk add --no-cache clang=${clang_version} git jq
+FROM alpine:3.12
+ENV clang_version=10.0.0-r2
+LABEL description="Clang Format ${clang_version}"
+RUN apk add --no-cache clang=${clang_version} git jq
 RUN apk add --no-cache clang git jq
 COPY .clang-format /.clang-format
 COPY entrypoint.sh /entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,18 +2,18 @@
 
 set -eu
 
-REPO_FULLNAME=$(jq -r ".repository.full_name" "$GITHUB_EVENT_PATH")
+# REPO_FULLNAME=$(jq -r ".repository.full_name" "$GITHUB_EVENT_PATH")
 
-echo "## Initializing git repo..."
-git init
-echo "### Adding git remote..."
-git remote add origin https://x-access-token:$GITHUB_TOKEN@github.com/$REPO_FULLNAME.git
-echo "### Getting branch"
-BRANCH=${GITHUB_REF#*refs/heads/}
-echo "### git fetch $BRANCH ..."
-git fetch origin $BRANCH
-echo "### Branch: $BRANCH (ref: $GITHUB_REF )"
-git checkout $BRANCH
+# echo "## Initializing git repo..."
+# git init
+# echo "### Adding git remote..."
+# git remote add origin https://x-access-token:$GITHUB_TOKEN@github.com/$REPO_FULLNAME.git
+# echo "### Getting branch"
+# BRANCH=${GITHUB_REF#*refs/heads/}
+# echo "### git fetch $BRANCH ..."
+# git fetch origin $BRANCH
+# echo "### Branch: $BRANCH (ref: $GITHUB_REF )"
+# git checkout $BRANCH
 
 echo "## Configuring git author..."
 git config --global user.email "clang-format@1337z.ninja"
